5de9d1093f647f6e44ec85276d5b563d5f3792a1
paramintrt94 <36721661+paramintrt94@users.noreply.github.com>
2021-05-25 13:52:09 -0400

    Test commit

---
+++ nodeServer.js
@@ -0,0 +1,64 @@
+const http = require("http");
+const fs = require("fs");
+const json = require("./data.json");
+var file;
+
+const server = http
+  .createServer(function (req, res) {
+    let file;
+
+    try {
+      file = fs.readFileSync("./index.html");
+    } catch (e) {
+      res.writeHead(404, { "content-type": "text/plain" });
+      res.write("404 File Not Found!");
+      res.end();
+      return;
+    }
+
+    if (file) {
+      res.writeHead(200, { "content-type": "text/html" });
+      res.write(file);
+      res.end();
+    }
+
+    /*******GETS THE FORM DATA************/
+    req.on("data", (data) => {
+      var arr = decodeURIComponent(data)
+        .replace(/\+/g, " ")
+        .replace("UserName=", "")
+        .replace("Email=", "")
+        .replace("message=", "")
+        .split("&");
+
+      var node = json.head;
+      var next;
+
+      /****TURNS JSON INTO LINKED LIST OF FORM INPUT********/
+      while (node) {
+        next = node.head;
+
+        if (node.head == null) {
+          node.head = {
+            name: arr[0],
+            email: arr[1],
+            message: arr[2],
+            head: null,
+          };
+
+          /**********WRITES THE NEW JSON TO THE JSON FILE****************/
+          fs.writeFile("./data.json", JSON.stringify(json, null, 2), (err) => {
+            if (err) {
+              throw err;
+            }
+          });
+          break;
+        } else {
+          node = next;
+        }
+      }
+    });
+  })
+  .listen(3000, () => {
+    console.log("Server running on 3000");
+  });
